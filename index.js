57e5e43b1f42e651acc7c4b9dcbf091f11d585a0
Mae Schreck <maeschreck@Maes-MBP.lan>
2022-09-20 15:14:17 -0400

    commit tree f2cd09d9a2b8c40fdae5d46ce2fd3f810c795129

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Tyler",
+    streetAddress: "6 Boulder lane",
+};
+function updateEmployeeWithKeyAndValue(employee, name, Mae){
+    const updateEmployeeWithKeyAndValue = {...employee};
+    updateEmployeeWithKeyAndValue[name] = Mae;
+    return updateEmployeeWithKeyAndValue;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, Sam){
+    //const destructivelyUpdateEmployeeWithKeyAndValue = (employee, "name", "Sam");
+    employee[name] = Sam;
+    return employee;
+}
+const newEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Sam");
+
+newEmployee;
+
+function deleteFromEmployeeByKey(employee, name, Tyler){
+    const deleteFromEmployeeByKey = {...updateEmployeeWithKeyAndValue};
+    delete deleteFromEmployeeByKey.name;
+    return deleteFromEmployeeByKey;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name, Tyler){
+   delete employee.name;
+   return employee;
+
+}
